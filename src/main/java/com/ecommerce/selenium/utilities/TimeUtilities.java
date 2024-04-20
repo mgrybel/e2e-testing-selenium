2e0937fae724d4af7c63ab3de25c08c6b2eefb01
mgrybel <grybel.m.m@gmail.com>
2024-04-20 23:35:48 +0200

    Updated TimeUtilities

--- src/main/java/com/ecommerce/selenium/utilities/TimeUtilities.java
+++ src/main/java/com/ecommerce/selenium/utilities/TimeUtilities.java
@@ -8,4 +8,12 @@ public class TimeUtilities {
     public static long getImplicitWait() {
         return Long.parseLong(PropertyReader.getInstance().getProperty(PropKey.IMPLICIT_WAIT.getPropVal()));
     }
+
+    /**
+     * Return the explicit wait
+     * @return
+     */
+    public static long getExplicitWait() {
+        return Long.parseLong(PropertyReader.getInstance().getProperty(PropKey.EXPLICIT_WAIT.getPropVal()));
+    }
 }
