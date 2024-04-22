1c41275ba76ce4176ae11e8df1e2510593850c12
mgrybel <grybel.m.m@gmail.com>
2024-04-22 14:09:59 +0200

    Updated the HtmlPageObject class

--- src/main/java/com/ecommerce/selenium/pages/HtmlPageObject.java
+++ src/main/java/com/ecommerce/selenium/pages/HtmlPageObject.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -87,5 +88,133 @@ public class HtmlPageObject extends DriverManager {
         } catch (TimeoutException e) {
             throw new TimeoutException();
         }
+    }
+
+    /**
+     * Enter text into a textbox
+     * @param element
+     * @param text
+     */
+    public void enterTextIntoTextBox(WebElement element, String text) {
+        element.clear();
+        element.sendKeys(text);
+    }
+
+    /**
+     * Method to click a link or button
+     * @param by
+     */
+    public void clickLinkOrBtn(By by) {
+        WebElement btnOrLink = findElementBy(by);
+        btnOrLink.click();
+    }
+
+    /**
+     * Method to click a link or button
+     * @param element
+     */
+    public void clickLinkOrBtn(WebElement element) {
+        element.click();
+    }
+
+    /**
+     * Method to return text
+     * @param by
+     * @return
+     */
+    public String getTextFromElement(By by) {
+        WebElement element = findElementBy(by);
+        return element.getText();
+    }
+
+    /**
+     * Method to return text
+     * @param element
+     * @return
+     */
+    public String getTextFromElement(WebElement element) {
+        return element.getText();
+    }
+
+    /**
+     * Method to return the attribute value
+     * @param by
+     * @param attr
+     * @return
+     */
+    public String getAttribute(By by, String attr) {
+        WebElement element = findElementBy(by);
+        return element.getAttribute(attr);
+    }
+
+    /**
+     * Method to return the attribute value
+     * @param element
+     * @param attr
+     * @return
+     */
+    public String getAttribute(WebElement element, String attr) {
+        return element.getAttribute(attr);
+    }
+
+    /**
+     * Method to select by visible text
+     * @param by
+     * @param textValue
+     */
+    public void selectByVisibleText(By by, String textValue) {
+        WebElement selectBox = findElementBy(by);
+        Select select = new Select(selectBox);
+        select.selectByVisibleText(textValue);
+    }
+
+    /**
+     * Method to select by visible text
+     * @param element
+     * @param textValue
+     */
+    public void selectByVisibleText(WebElement element, String textValue) {
+        Select select = new Select(element);
+        select.selectByVisibleText(textValue);
+    }
+
+    /**
+     * Method to select checkbox
+     * @param by
+     * @return
+     */
+    public boolean selectCheckBox(By by) {
+        WebElement webElement = findElementBy(by);
+        if (webElement.isSelected()) {
+            return true;
+        }
+        webElement.click();
+        return true;
+    }
+
+    /**
+     * Method to select checkbox
+     * @param webElement
+     * @return
+     */
+    public boolean selectCheckBox(WebElement webElement) {
+        if (webElement.isSelected()) {
+            return true;
+        }
+        webElement.click();
+        return true;
+    }
+
+    /**
+     * Method to check if text is present on page
+     * @param text
+     * @return
+     */
+    public boolean isTextPresentOnPage(String text) {
+        String pageSource = driver.getPageSource();
+        if (pageSource.contains(text)) {
+            return true;
+        }
+        return false;
     }
 }
