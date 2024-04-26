8835075369baf7108481e800d188e7d7efabfc71
mgrybel <grybel.m.m@gmail.com>
2024-04-26 08:18:11 +0200

    Updated login tests

--- src/test/java/com/ecommerce/selenium/tests/LoginTest.java
+++ src/test/java/com/ecommerce/selenium/tests/LoginTest.java
@@ -3,13 +3,18 @@ package com.ecommerce.selenium.tests;
 import com.ecommerce.selenium.steps.EcommerceWebsiteSteps;
 import io.qameta.allure.Description;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
+import static com.ecommerce.selenium.tags.EcommerceWebsiteTags.SMOKE;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@ExtendWith(TestListener.class)
 @DisplayName("Login Tests")
 public class LoginTest extends EcommerceWebsiteSteps {
+    @Tag(SMOKE)
     @Test
     @DisplayName("Log in to the app with valid customer credentials")
     @Description("Log in to the app as a customer")
